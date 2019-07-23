3c943fadc485fc406bd36575a1541a695a013d70
zombe14 <zombe14@naver.com>
2019-07-23 16:18:29 +0900

    .

---
+++ src/main/java/com/project/weekend/AdminController.java
@@ -0,0 +1,56 @@
+package com.project.weekend;
+
+public class AdminController {
+	
+	//////////////user 관리; //////////////
+	// user list;
+	public void userList() throws Exception{}
+	
+	// user info;
+	public void userSelect() throws Exception{}
+	
+	// user up/down grade;
+	public void userUpdate() throws Exception{}
+	
+	// user delete;
+	public void userDelete() throws Exception{}
+	
+	////////////// board 관리; //////////////
+	
+	// notice;
+	public void aNoticeList() throws Exception{}
+	
+	// show;
+	public void aShowList() throws Exception{}
+	
+	// festival
+	public void aFestiList() throws Exception{}
+	
+	// after;
+	public void aAfterList() throws Exception{}
+
+	// recommend;
+	public void aRecommendList() throws Exception{}
+	
+	// rank;
+	public void aRankList() throws Exception{}
+	
+	// qna;
+	public void aQnaList() throws Exception{}
+	
+	////////////// reservation 관리; //////////////
+	// reservation;
+	// reserList;
+	public void aReserList() throws Exception{}
+	
+	// reserInfo;
+	public void aReserSelect() throws Exception{}
+	
+	// reserUpdate;
+	public void aReserUpdate() throws Exception{}
+	
+	// reserDelete;
+	public void areserDelete() throws Exception{}
+	
+	
+}
