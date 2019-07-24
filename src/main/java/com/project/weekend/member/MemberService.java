a8a1bcc56a8937c94f92b49a2b70209406d64835
haptism <haptism007@gmail.com>
2019-07-24 15:29:02 +0900

    dao

---
+++ src/main/java/com/project/weekend/member/MemberService.java
@@ -0,0 +1,24 @@
+package com.project.weekend.member;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+	
+	@Inject
+	private MemberDAO memberDAO;
+	
+	
+	public int setWrite(MemberDTO memberDTO, HttpSession session)throws Exception{
+		int result = memberDAO.setWrite(memberDTO);
+		return result;
+	}
+	
+	public MemberDTO getSelect(MemberDTO memberDTO)throws Exception{
+		return memberDAO.getSelect(memberDTO);
+	}
+	
+}
