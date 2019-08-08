850387b244264666f07d04e601edd2751bc4a041
ssseulan222 <ssseulan222@gmail.com>
2019-08-08 14:14:16 +0900

    db column 변경
    1. comments : num->anum 변경
    2. festi : age (연령제한) 추가
    3. festi : category number(1) -> (2) 변경

--- src/main/java/com/project/weekend/board/comments/CommentsDTO.java
+++ src/main/java/com/project/weekend/board/comments/CommentsDTO.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 public class CommentsDTO {
 	private int cnum;
-	private String num;
+	private String anum;
 	private String contents;
 	private Date reg_date;
 	private String writer;
@@ -38,10 +38,10 @@ public class CommentsDTO {
 		this.cnum = cnum;
 	}
 	public String getNum() {
-		return num;
+		return anum;
 	}
-	public void setNum(String num) {
-		this.num = num;
+	public void setNum(String anum) {
+		this.anum = anum;
 	}
 	public String getContents() {
 		return contents;
