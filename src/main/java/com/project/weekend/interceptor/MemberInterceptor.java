31b0272e7496804cce187808a9bbdb6f9b1d5ac5
haptism <haptism007@gmail.com>
2019-08-14 12:46:50 +0900

    dd

--- src/main/java/com/project/weekend/interceptor/MemberInterceptor.java
+++ src/main/java/com/project/weekend/interceptor/MemberInterceptor.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class memberInterceptor extends HandlerInterceptorAdapter {
+public class MemberInterceptor extends HandlerInterceptorAdapter {
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)throws Exception{
